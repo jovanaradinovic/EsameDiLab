d0cd66e5db7d9eee5366daf3ec07280fca3345b9
Jovana Radinovic <65192121+jovanaradinovic@users.noreply.github.com>
2022-02-07 13:50:21 +0100

    Add files via upload
    Base dell'esame

---
+++ esame.py
@@ -0,0 +1,15 @@
+class ExamException(Exception): #classe per le eccezioni
+
+    pass
+ 
+class CSVTimeSeriesFile:
+    def __init__ (self,name):
+        pass
+    def get_data(self):
+        pass
+
+def compute_avg_monthly_difference(time_series,first_year,last_year):
+    pass
+def main():
+    pass
+
